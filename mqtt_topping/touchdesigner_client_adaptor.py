1be2079c9489956599b3dc48de044c5f5eced7ca
Julian Krumow <julian.krumow@artcom.de>
2026-07-20 14:16:19 +0200

    Update api.

--- mqtt_topping/touchdesigner_client_adaptor.py
+++ mqtt_topping/touchdesigner_client_adaptor.py
@@ -12,7 +12,7 @@ class TouchDesignerClientAdaptor(MqttClientAdaptor):
         super(TouchDesignerClientAdaptor, self).__init__()
         self.client = client
 
-    def connect(self, host: str, port: int, client_id: str, on_connect: any = None):
+    def connect(self, host: str, port: int, client_id: str, on_connect: any = None, on_connect_fail: any = None, on_disconnect: any = None):
         self.client.par.active = True
         self.client.par.reconnect.pulse()
 
